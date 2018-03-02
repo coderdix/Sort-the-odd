0504e98a15345479936d9a48d40e8b73a1941f9c
Richard Richardson <rmrichardson24@gmail.com>
2018-03-02 03:20:02 -0600

    Refactored - much better solution!

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
 function sortArray(array) {
-  // Return empty array if array is empty
-  if (array == 0) return array
-  
-  // Define vars & extract odd / even sets 
-  let emptyArray = [], oddArray, output,
-      oddNumbers = array.filter(num => num % 2 !== 0),
-      evenNumbers = array.filter(num => num % 2 === 0),
-      evenPositions = array.map(getPosition)
-  
-  // Sort odd numbers & output to new array 
-  oddNumbers.sort((a,b) => a-b)
-  oddArray = emptyArray.concat(oddNumbers)
-  
-  // Cb fn to get position of even numbers 
-  function getPosition(number, index, array) {
-    let positions = array.indexOf(evenNumbers[index], index)
-    return positions
-  }
-  evenPositions = evenPositions.slice(0, evenNumbers.length)
-  
-  // Add even numbers to the sorted array of odd numbers 
-  output = oddArray.map(addEvenNumbers)
-  function addEvenNumbers(number, index, array) {
-    array.splice(evenPositions[index], 0, evenNumbers[index])
-    return array.filter(value => value !== undefined)
-  }
-  
-  // Add zero back to its position within the array
-  let zeroPosition = array.indexOf(0)
-  if (zeroPosition === -1) return output[output.length - 1]
-  output[0].splice(zeroPosition, 0, 0)
-
-  // Output the final value 
-  return output[output.length - 1]
+  const oddArray = array
+    .map(number => number !== 0 && number % 2 !== 0 ? number : undefined)
+    .filter(truthyValues => truthyValues)
+    .sort((a,b) => a-b);
+  let index = 0;
+  return array
+.map(number => number === 0 || number % 2 === 0
+  ? number
+  : oddArray[index++]);
 }
 
-sortArray([1, 92, 30, 80, 92, 1, 5, 2, 58, 40, 86, 7, 56, 68, 11, 27, 72, 49, 26, 75, 24, 77, 87, 93, 56, 97, 84, 94, 76, 40])
+console.log(sortArray([1, 92, 30, 80, 92, 1, 5, 2, 58, 40]));
