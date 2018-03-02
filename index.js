f298a2936425249ce3cffd039487b95109dae64d
Richard Richardson <rmrichardson24@gmail.com>
2018-03-01 19:03:00 -0600

    Correct for duplicate even numbers by adding the val of index as place to start the indexOf search within the iteration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function sortArray(array) {
   // Define vars & extract odd / even sets 
   let emptyArray = [], oddArray, output,
       oddNumbers = array.filter(num => num % 2 !== 0),
-      evenNumbers = array.filter(num => num % 2 === 0 && num !== 0),
+      evenNumbers = array.filter(num => num % 2 === 0),
       evenPositions = array.map(getPosition)
   
   // Sort odd numbers & output to new array 
@@ -14,7 +14,7 @@ function sortArray(array) {
   
   // Cb fn to get position of even numbers 
   function getPosition(number, index, array) {
-    let positions = array.indexOf(evenNumbers[index])
+    let positions = array.indexOf(evenNumbers[index], index)
     return positions
   }
   evenPositions = evenPositions.slice(0, evenNumbers.length)
@@ -29,10 +29,10 @@ function sortArray(array) {
   // Add zero back to its position within the array
   let zeroPosition = array.indexOf(0)
   if (zeroPosition === -1) return output[output.length - 1]
-  output[output.length - 1].splice(zeroPosition, 0, 0)
+  output[0].splice(zeroPosition, 0, 0)
 
   // Output the final value 
   return output[output.length - 1]
 }
 
-console.log(sortArray([2, 22, 1, 5, 4, 11, 37, 0]))
+sortArray([1, 92, 30, 80, 92, 1, 5, 2, 58, 40, 86, 7, 56, 68, 11, 27, 72, 49, 26, 75, 24, 77, 87, 93, 56, 97, 84, 94, 76, 40])
